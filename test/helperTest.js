cd645d3318723d7930b43eee8624dc3dd709fc79
Sam <sammynarduzzi@gmail.com>
2023-01-26 23:32:38 -0800

    finish helperTest and add README

--- test/helperTest.js
+++ test/helperTest.js
@@ -1,24 +1,29 @@
-// const { assert } = require('chai');
+const { assert } = require('chai');
 
-// const { getUserByEmail } = require('../helpers.js');
+const { getUserByEmail } = require('../helpers.js');
 
-// const testUsers = {
-//   "userRandomID": {
-//     id: "userRandomID", 
-//     email: "user@example.com", 
-//     password: "purple-monkey-dinosaur"
-//   },
-//   "user2RandomID": {
-//     id: "user2RandomID", 
-//     email: "user2@example.com", 
-//     password: "dishwasher-funk"
-//   }
-// };
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
 
-// describe('getUserByEmail', function() {
-//   it('should return a user with valid email', function() {
-//     const user = getUserByEmail("user@example.com", testUsers)
-//     const expectedUserID = "userRandomID";
-//     // Write your assert statement here
-//   });
-// });
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers);
+    const expectedUserID = "userRandomID";
+    assert.equal(user, expectedUserID);
+  });
+  it('Should return null when there is an invalid email or no email is found', function() {
+    const user = getUserByEmail("user@example.com", testUsers);
+    const expectedUserID = null;
+    assert.equal(user, expectedUserID);
+  });
+});
